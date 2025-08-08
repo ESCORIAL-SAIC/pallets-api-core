f7df872fb9eecfbe4d8872dbbad47b64219d3f39
maurinicolas <mlopez@escorial.com.ar>
2025-08-08 09:23:05 -0300

    añadido clase para tabla public.aux_controlcalidad

---
+++ PalletsApiCore/Models/aux_controlcalidad.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PalletsApiCore.Models
+{
+    [Table("aux_controlcalidad", Schema = "public")]
+    public partial class aux_controlcalidad
+    {
+        [Key]
+        public int etiqueta { get; set; }
+        public Guid reparador_empleado_id{ get; set; }
+        public string reparador_empleado_n { get; set; }
+        public bool reparador_estado { get; set; }
+        public Guid reparador_falla_id { get; set; }
+        public string reparador_falla_n { get; set; }
+    }
+}
